77bdf24f9be3af8a0e647affefe583235bfa5e3d
Sandernobel <sandernobel@live.nl>
2020-05-19 14:34:26 +0200

    Cross validation doet het nu!

--- Assignment 2/train_model.py
+++ Assignment 2/train_model.py
@@ -1,8 +1,9 @@
 import lightgbm as lgbm
 import numpy as np
 import pandas as pd
-import pickle, datetime
+import pickle
 from collections import Counter
+from sklearn.model_selection import KFold
 
 
 def split_data(df: pd.DataFrame):
@@ -10,14 +11,15 @@ def split_data(df: pd.DataFrame):
     Splits data based on search id's
     :return:
     """
+
+    kf = KFold(n_splits=5) # 5 splits ipv 10 want het duurt al best lang om te trainen en het zijn grote groepen
     unique_srch_ids = df['srch_id'].unique()
-    train_ids = np.random.choice(unique_srch_ids, size=round(0.8*len(unique_srch_ids)), replace=False)
-    val_ids = np.setdiff1d(unique_srch_ids, train_ids)
 
-    train_data = df.loc[df['srch_id'].isin(train_ids)]
-    val_data = df.loc[df['srch_id'].isin(val_ids)]
-
-    return train_data, val_data
+    train_ids, val_ids = [], []
+    for train_index, val_index in kf.split(unique_srch_ids):
+        train_ids.append(unique_srch_ids[train_index])
+        val_ids.append(unique_srch_ids[val_index])
+    return np.asarray(train_ids), np.asarray(val_ids)
 
 
 def get_predictions(df: pd.DataFrame, model: lgbm.LGBMRanker):
@@ -36,13 +38,14 @@ def get_predictions(df: pd.DataFrame, model: lgbm.LGBMRanker):
     print(f'\t Writing answers to csv')
     kaggle_answer.to_csv('expedia_answer.csv', index=False)
 
-def save_model(model: lgbm.LGBMRanker):
+def save_model(model: lgbm.LGBMRanker, cv_scores: list):
     """
     Saves model
     :param model:
     :return:
     """
-    out_file = open("model.p", 'wb+')
+
+    out_file = open("model_"+str(np.mean(cv_scores)), 'wb+')
     pickle.dump(model, out_file)
     out_file.close()
 
@@ -55,16 +58,29 @@ def train_lgbm(df: pd.DataFrame):
     """
 
     print("\tSplitting data")
-    train_data, val_data = split_data(df.drop(['click_bool', 'booking_bool', 'position'], axis=1))
-    y_train, y_val = train_data['relevance'], val_data['relevance']
-    X_train, X_val = train_data.drop('relevance', axis=1), val_data.drop('relevance', axis=1)
+    df.drop(['click_bool', 'booking_bool', 'position'], axis=1, inplace=True)
+    train_ids, val_ids = split_data(df)
+    cv_scores = []
 
-    train_queries = list(Counter(np.asarray(X_train['srch_id'])).values())
-    val_queries = list(Counter(np.asarray(X_val['srch_id'])).values())
+    for i, train_id in enumerate(train_ids):
 
-    gbm = lgbm.LGBMRanker()
-    print(f"\tTraining LGBM Ranker")
-    print(gbm.fit(X_train, y_train, group=train_queries,
-        eval_set=[(X_val, y_val)], eval_group = [val_queries],
-        eval_at=[5], early_stopping_rounds=50))
+        train_data = df.loc[df['srch_id'].isin(train_id)]
+        val_data = df.loc[df['srch_id'].isin(val_ids[i])]
+
+        y_train, y_val = train_data['relevance'], val_data['relevance']
+        X_train, X_val = train_data.drop('relevance', axis=1), val_data.drop('relevance', axis=1)
+
+        train_queries = list(Counter(np.asarray(X_train['srch_id'])).values())
+        val_queries = list(Counter(np.asarray(X_val['srch_id'])).values())
+
+        gbm = lgbm.LGBMRanker()
+        print(f"\tTraining LGBM Ranker, fold {i+1}")
+        gbm.fit(X_train, y_train, group=train_queries,
+            eval_set=[(X_val, y_val)], eval_group = [val_queries],
+            eval_at=[5], early_stopping_rounds=50)
+        print('\n')
+        cv_scores.append(gbm.best_score_['valid_0']['ndcg@5'])
+
+    print(cv_scores)
+    save_model(gbm, cv_scores)
     return gbm
